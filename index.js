68e547f0a4df36a130d6239d7340c72dc21e0eb8
Mike McNeil <mike@balderdash.co>
2014-12-08 17:39:51 -0600

    Fix index.js

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 // Export Machine.pack() object for convenience
-var path = require('path');
-
 module.exports = require('machine').pack({
-  pkg: require('./package.json')
+  pkg: require('./package.json'),
+  dir: __dirname
 });
